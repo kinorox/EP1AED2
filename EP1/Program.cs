f713bd276cd5238d2ee1db516ebca8ceceec08a3
Gabriel Bombardi <gabriel.bombardi@gmail.com>
2020-05-23 19:19:31 -0300

    adding solution

---
+++ EP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
